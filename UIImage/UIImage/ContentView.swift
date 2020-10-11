655442b641936003255e6a6c1ba2eb33bd45aff6
Le <nguyenngocle2408@gmail.com>
2020-10-11 10:43:44 +0700

    Update UIImage.

--- UIImage/UIImage/ContentView.swift
+++ UIImage/UIImage/ContentView.swift
@@ -15,6 +15,13 @@ struct ContentView: View {
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.blue, lineWidth: 3))
             .shadow(radius: 10)
+        Image("b")
+            .resizable()
+            .frame(width: 200, height: 200, alignment: .bottomLeading)
+            .clipShape(Circle())
+            .overlay(Circle()
+                        .stroke(Color.blue))
+            .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: 1.0, y: 4.0)
     }
 }
 
