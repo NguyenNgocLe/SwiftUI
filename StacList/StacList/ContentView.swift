0e165eb72950fcebdb3b3f92088c50341d7f66b7
Le <nguyenngocle2408@gmail.com>
2020-10-18 12:46:49 +0700

    .

--- StacList/StacList/ContentView.swift
+++ StacList/StacList/ContentView.swift
@@ -21,8 +21,14 @@ struct ContentView: View {
     var body: some View {
         Form(content: {
             StudentRow(name: "SwiftUI")
+                .foregroundColor(Color.red)
+                .padding(5)
             StudentRow(name: "Javascript")
+                .foregroundColor(Color.green)
+                .padding(5)
             StudentRow(name: "Autolayout")
+                .foregroundColor(Color.blue)
+                .padding(5)
         })
     }
 }
