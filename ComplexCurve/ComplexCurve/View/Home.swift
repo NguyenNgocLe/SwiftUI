6ce60faf9f3b55c1baddfa8fcc9971ea44659ded
Le <nguyenngocle2408@gmail.com>
2020-11-09 22:54:36 +0700

    Edit UIButton

--- ComplexCurve/ComplexCurve/View/Home.swift
+++ ComplexCurve/ComplexCurve/View/Home.swift
@@ -15,15 +15,45 @@ struct Home: View {
     
     var body: some View {
         VStack {
-            Text("EASTHER!")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
-            
+            VStack {
+                Text("EASTHER!")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                    .padding()
+                Spacer()
+                Text("STATUS")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                Text("DISCONNECTED")
+                    .font(.title)
+                    .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    .padding(.top, 5)
+            }.frame(height: UIScreen.main.bounds.height / 3.3)
+            // Button....
+            Button(action: {
+                
+            }, label: {
+                VStack(spacing: 15) {
+                    Image(systemName: "power")
+                        .font(.system(size: 70))
+                        .foregroundColor(Color("power"))
+                    
+                    Text("START")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                }
+                .padding(50)
+                .background(
+                    LinearGradient(gradient: .init(colors: [Color("pgradient1"), Color("pgradient2")]), startPoint: .leading, endPoint: .trailing)
+                ).clipShape(Circle())
+            })
+            //
             Spacer()
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(
-            LinearGradient(gradient: .init(colors: gradient1), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: .init(colors: gradient), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
         )
         
